4a6835ae921dcad3817ccbb558ce94b4f8b61d2a
Steven Prichard <spprichard20@gmail.com>
2019-12-26 13:01:08 -0600

    Hydrate Teams Table
    - Refactored naming of files to match other services
    - Updated MLBScrapperLib dep to allow extensions
    - First pass at hydrating teams table via POST request
    - Added new endpoint to trigger hydration

--- Sources/App/Controllers/TeamController.swift
+++ Sources/App/Controllers/TeamController.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  TeamController.swift
 //  
 //
 //  Created by Steven Prichard on 12/24/19.
@@ -12,9 +12,25 @@ import MLBScrapperLib
 struct TeamController: RouteCollection {
     func boot(router: Router) throws {
         router.get("api", "teams", use: getAllHandler)
+        router.post("api", "teams", use: hydrateHandler)
     }
     
     func getAllHandler(_ req: Request) throws -> Future<[Team]> {
         return Team.query(on: req).all()
+    }
+    
+    func hydrateHandler(_ req: Request) throws -> Future<[Team]> {
+        return try req
+            .content
+            .decode(HydrateRequest.self)
+            .flatMap(to: [Team].self, ({ hydrate in
+                let teamList = try Team.GetTeams(season: hydrate.year)
+                return teamList
+                    .map({ $0.create(on: req) })
+                    .flatten(on: req)
+            }))
+            .catchMap({ error in
+                throw Abort(.internalServerError, reason: "\(error)")
+            })
     }
 }
---
+++ Sources/App/Models/Models.swift
@@ -0,0 +1,13 @@
+//
+//  Models.swift
+//  
+//
+//  Created by Steven Prichard on 12/25/19.
+//
+
+import Foundation
+import MLBScrapperLib
+
+struct HydrateRequest: Codable {
+    var year: Int
+}
