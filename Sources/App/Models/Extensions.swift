7874cd62077a0f3bce58f785e9186a36705a0c87
Steven Prichard <spprichard20@gmail.com>
2019-12-29 17:55:09 -0600

    Update: Remove Custom Migration
    - Remove migration
    - Migration not needed with new version of Lib
    - Use of MLBLookupTeam allows for Team type to not need a custom migration

--- Sources/App/Models/Extensions.swift
+++ Sources/App/Models/Extensions.swift
@@ -13,19 +13,4 @@ import MLBScrapperLib
 extension Team: PostgreSQLModel {}
 extension Team: Migration {}
 extension Team: Content {}
-
-extension Team: PostgreSQLMigration {
-    public static func prepare(on conn: PostgreSQLConnection) -> EventLoopFuture<Void> {
-        return PostgreSQLDatabase.create(Team.self, on: conn) { builder in
-            builder.field(for: \.id, isIdentifier: true)
-            builder.field(for: \.VenueID)
-            builder.field(for: \.VenueName)
-            builder.field(for: \.Name)
-            builder.field(for: \.DivisionID)
-        }
-    }
-    
-    public static func revert(on connection: PostgreSQLConnection) -> EventLoopFuture<Void> {
-        return PostgreSQLDatabase.delete(Team.self, on: connection)
-    }
-}
+extension Team: Model {}
